801c97884b8b7e071923a83ed186de71b9240dce
sa xiao <sa.xiao2009@gmail.com>
2012-12-24 11:42:36 -0800

    First version of Hangman game.

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,9 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+angular.module('hangman', ['hangman.filters', 'hangman.services', 'hangman.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.when('/welcome', {templateUrl: 'partials/welcome.html', controller: WelcomeCtrl});
+    $routeProvider.when('/game', {templateUrl: 'partials/game.html', controller: GameCtrl});
+    $routeProvider.otherwise({redirectTo: '/welcome'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,10 +3,82 @@
 /* Controllers */
 
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
-
-
-function MyCtrl2() {
+function WelcomeCtrl() {
 }
-MyCtrl2.$inject = [];
+//MyCtrl1.$inject = ['$scope', '$http'];
+
+
+function GameCtrl($scope, Problem) {
+  var problems = Problem.query(function(problems){
+     chooseProblemAtRandom(); 
+     initDisplayedPhrase(); 
+  });
+
+  $scope.pickProblem = function(){
+    chooseProblemAtRandom(); 
+    initDisplayedPhrase();
+  }
+  
+  var chooseProblemAtRandom = function(){
+    var randomnumber=Math.floor(Math.random()*problems.length);
+    $scope.pickedProblem = problems[randomnumber];
+   
+  }
+
+ var initDisplayedPhrase = function(){
+    $scope.displayedPhrase = $scope.pickedProblem.phrase.split('');
+    for(var i=0; i<$scope.displayedPhrase.length; i++){
+      var letter = $scope.displayedPhrase[i].toUpperCase();
+      if(letter >= "A" && letter <= "Z"){
+        $scope.displayedPhrase[i]='_';
+      }
+    }    
+    $scope.errorLeft = $scope.pickedProblem.errorallowed;
+    $scope.gameStatus = "inGame";
+    $scope.selected = new Object();
+  }
+
+  $scope.updateDisplay = function(input){
+    if(!$scope.selected[input]){
+      $scope.selected[input]=true;
+      if($scope.gameStatus == "inGame"){
+        var hit = false;
+        for(var i=0; i<$scope.displayedPhrase.length; i++){
+          var letter = $scope.displayedPhrase[i];
+          if(letter == '_' && $scope.pickedProblem.phrase.charAt(i).toUpperCase() == input.toUpperCase()){
+            $scope.displayedPhrase[i]=$scope.pickedProblem.phrase.charAt(i);
+            hit = true;
+          }
+        }
+        if(hit && isWin()){
+          $scope.gameStatus = "win";        
+        }
+        if(!hit){
+          $scope.errorLeft--;
+          if($scope.errorLeft == 0){
+            $scope.gameStatus = "fail";
+          } 
+        }
+      }
+    }
+  }
+
+  $scope.showAnswer = function(){
+     $scope.displayedPhrase = $scope.pickedProblem.phrase.split('');
+  }
+  
+  $scope.getStatusText = function(){
+    if($scope.gameStatus == "win"){
+      return "You got it! Congrats!";
+    }
+    if($scope.gameStatus == "fail"){
+      return "Sorry, try again!";
+    }
+  }
+
+  var isWin = function(){
+    return $scope.displayedPhrase.toString() == $scope.pickedProblem.phrase.split('').toString();
+  }
+ 
+}
+//MyCtrl2.$inject = ['$scope', 'Problem'];
